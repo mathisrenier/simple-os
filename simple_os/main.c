52012876714da205ba7798725f595c55f0055ee6
Mathis Renier <reniermathis@gmail.com>
2021-02-10 13:12:33 -0500

    Initial Commit

---
+++ simple_os/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  simple_os
+//
+//  Created by Mathis Renier on 2021-02-10.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
